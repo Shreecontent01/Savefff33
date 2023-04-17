5a6af56fdb1ec135fdf1118aaff7569d2a57e6c5
ArghyadipBiswas <107795593+ArghyadipBiswas@users.noreply.github.com>
2023-04-17 23:37:12 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -16,7 +16,7 @@ API_ID = config("API_ID", default=7868296, cast=int)
 API_HASH = config("API_HASH", default="bfa7fecb33443243ff8c8b0f27f9cd68")
 BOT_TOKEN = config("BOT_TOKEN", default="5394356797:AAGoTNSZZqmDwzT0Bqn1APDZ5_0ueUb4DMo")
 SESSION = config("SESSION", default="BABGjQlHSLZ11dA2F-YML5VmdMngcJ9R9Gv8dGkS0Xy5guZEk5yFehxulSGygvNbCgj0lK8KIo61t9Hhg4FshbzTr7pkvRtcm-kuUzxnTz8CCJs7HoZrVRvhCxZ_PYzVj2JoSP9XONRFHMdlDtOKh9DE6fatul58rAhR888rfwPHaSNNCgvHVuA34jJXFMaLQT_T2JHFSW_eAVSqwky4-yet-DPprFQ8-X8bY7BvY6jWtrJNgMoVjy7SlnT36qLI9k2m-PEzT59Tf7ZzRvP5UMi4fghIcPhRWQVdVwBCvPFoJI8z_QqNkalUDWHIzDYDaH8-swF5tBR_QyTdW_Sw7DFOAAAAASq_dygA")
-FORCESUB = config("FORCESUB", default="vfyugvuyvguyyjg")
+FORCESUB = config("FORCESUB", default=-1001870063718)
 AUTH = config("AUTH", default=5042525177, cast=int)
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
